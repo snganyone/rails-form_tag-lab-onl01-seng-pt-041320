3211fb40c3b7c23597f4ab155c35a189e084370d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-25 20:41:29 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,9 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new
+  def new 
+  end
+
+  def create
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to new_student_path
+    
+    redirect_to students_path
   end
 
 end
